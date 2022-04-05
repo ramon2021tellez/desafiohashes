01fb199ed7f29906b35f21b025029c797fdc9d2a
ramon2021tellez <miranda.tellez@gmail.com>
2022-04-05 19:21:31 -0400

    second commit

---
+++ iter3.rb
@@ -0,0 +1,21 @@
+ventas = {
+Enero: 15000,
+Febrero: 22000,
+Marzo: 12000,
+Abril: 17000,
+Mayo: 81000,
+Junio: 13000,
+Julio: 21000,
+Agosto: 41200,
+Septiembre: 25000,
+Octubre: 21500,
+Noviembre: 91000,
+Diciembre: 21000
+}
+def filter (hash ,reference)
+hash.select do |key , value|
+value > reference
+end
+end
+
+print filter(ventas ,ARGV[0].to_i)
